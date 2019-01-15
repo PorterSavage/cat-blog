84a5f92046e42833ff281d1af64471ee60a55129
Porter Savage and Theary Im <porter@>
2019-01-15 15:10:04 -0800

    add more functions to highlight when click on a paragraph

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
     $("button").click(function() {
       $("body").toggleClass("dark-background");
     });
+
+    $(".highlight").click(function() {
+      $(".highlight").toggleClass("highlightTwo");
+    });
+
+    // $(".highlightTwo").click(function() {
+    //   $(".highlight").removeClass("highlightTwo");
+    // });
+
+
 });
